80fdf93488ab730a3a7715cf024bc0f88e980ca8
Amlsakr <amlsakr9@gmail.com>
2026-03-09 10:08:46 +0200

    Add unit test for HomeCubit

---
+++ test/home/home_cubit_test.dart
@@ -0,0 +1,37 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:book_store_app_bloc_cubit/core/data/api_response.dart';
+import 'package:book_store_app_bloc_cubit/domain/use_cases/home/home_use_case.dart';
+import 'package:book_store_app_bloc_cubit/ui/home/controllers/home_cubit.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockHomeUseCase extends Mock implements HomeUseCase {}
+
+void main() {
+  MockHomeUseCase homeUseCase = MockHomeUseCase();
+  HomeCubit homeCubit = HomeCubit(homeUseCase);
+
+  blocTest<HomeCubit, HomeState>(
+    'emits homeDataSuccess when books are fetched successfully',
+    build: () {
+      when(
+        () => homeUseCase.execute(),
+      ).thenAnswer((_) async => ApiResponse.success([]));
+      return homeCubit;
+    },
+    act: (homeCubit) => homeCubit.getListOfBooks(),
+    expect: () => [isA<HomeDataSuccess>()],
+  );
+
+  blocTest(
+    'emits homeError when usecase return error',
+    build: () {
+      when(
+        () => homeUseCase.execute(),
+      ).thenAnswer((_) async => ApiResponse.error("Api Error"));
+      return homeCubit;
+    },
+    act: (homeCubit) => homeCubit.getListOfBooks(),
+    expect: () => [isA<HomeError>()],
+  );
+}
